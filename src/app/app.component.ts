9f1d78a6906f398a42391edd5f235565d21823ac
Józef Bossowski <bossowski92@gmail.com>
2018-10-11 21:39:28 +0200

    Dodane pełne funkcjonowanie menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,48 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component, ViewChild} from '@angular/core';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {MenuController, Nav, Platform} from 'ionic-angular';
 
-import { AboutPage } from '../pages/about/about';
-@Component({
-  templateUrl: 'app.html'
-})
+import {AboutPage} from '../pages/about/about';
+import {FavouritesPage} from '../pages/favourites/favourites';
+import {MapPage} from '../pages/map/map';
+import {RankPage} from '../pages/rank/rank';
+import {SearchPage} from '../pages/search/search';
+
+@Component({templateUrl : 'app.html'})
 export class MyApp {
-  rootPage:any = AboutPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = AboutPage;
+  pages: Array<{title : string, component: any}>;
+
+  constructor(public platform: Platform, public menu: MenuController,
+              public statusBar: StatusBar, public splashScreen: SplashScreen) {
+
+    this.initializeApp();
+
+    this.pages = [
+      {title : 'O nas!', component : AboutPage},
+      {title : 'Ulubione', component : FavouritesPage},
+      {title : 'Mapa', component : MapPage},
+      {title : 'Ranking', component : RankPage},
+      {title : 'Szukaj', component : SearchPage},
+    ];
+  }
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
   }
 }

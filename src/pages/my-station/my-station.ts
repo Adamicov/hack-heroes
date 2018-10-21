e9fbaa2c71754333c0c3c639c06cdc02dc92f133
Adamico <adammweber@o2.pl>
2018-10-21 17:16:13 +0200

    station service

--- src/pages/my-station/my-station.ts
+++ src/pages/my-station/my-station.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation'
+import {GeoCoord} from '../../providers/haversine/geocoord';
+import { StationObj } from '../../models/stationObj';
+import { RestProvider } from '../../providers/rest/rest';
+import { HaversineProvider } from '../../providers/haversine/haversine';
 
 /**
  * Generated class for the MyStationPage page.
@@ -15,11 +20,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MyStationPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  stations: StationObj[] = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation, public restProvider: RestProvider, public haversineService: HaversineProvider) {
+    
+   
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MyStationPage');
+  getNearestPollution(){
+    let nearestStation = this.getNearestStation();
   }
+
+  getNearestStation(){
+    let station: StationObj = this.stations[0];
+    let myLocalization = this.getMyLocalization();
+    let max = this.getInMeters(myLocalization, new GeoCoord(station.latitude, station.longitude));
+    for (let i = 1; i < this.stations.length; i++){
+      let state = this.stations[i];
+      let value = this.getInMeters(myLocalization, new GeoCoord(state.latitude, state.longitude))
+      if (value > max){
+        max = value;
+        station = state;
+      }
+    }
+    return station;
+  }
+
+  getMyLocalization(){
+    let my: GeoCoord;
+    this.geolocation.getCurrentPosition().then(pos => {
+      my = new GeoCoord(pos.coords.latitude, pos.coords.longitude);
+    })
+    return my;
+  }
+
+  getInMeters(my: GeoCoord, stat: GeoCoord){
+    return this.haversineService.getDistanceInMeters(my, stat);
+  }
+
 
 }

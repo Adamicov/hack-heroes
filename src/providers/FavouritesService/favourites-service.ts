990d67e1b8edd50dc42e9e166b2c95890cbb3979
Józef Bossowski <bossowski92@gmail.com>
2018-10-21 13:02:25 +0200

    About note and MyStation Page

--- src/providers/FavouritesService/favourites-service.ts
+++ src/providers/FavouritesService/favourites-service.ts
@@ -1,25 +1,23 @@
-import {NativeStorage} from '@ionic-native/native-storage';
 import {Injectable} from '@angular/core';
 
 @Injectable()
 export class FavouritesService{
   private favourites: any[] = [];
 
-    constructor (private nativeStorage: NativeStorage) {}
+    constructor () {}
 
     addFavourite(station){
       this.favourites.push(station);
-      this.nativeStorage.setItem('favourites', this.favourites);
     }
 
     getFavourites(){
-        return this.nativeStorage.getItem('favourites')
+        /*return this.nativeStorage.getItem('favourites')
           .then(
             (favourites) => {
               this.favourites = favourites == null ? [] : favourites;
               return this.favourites.slice();
             }
-          )
+          )*/
     }
 
 

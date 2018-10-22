5dc98315e4d38cae4a82e6b8757871222d9e0114
Adamico <adammweber@o2.pl>
2018-10-22 20:11:42 +0200

    pls dzialaj

--- src/pages/my-station/my-station.ts
+++ src/pages/my-station/my-station.ts
@@ -5,6 +5,7 @@ import {GeoCoord} from '../../providers/haversine/geocoord';
 import { StationObj } from '../../models/stationObj';
 import { RestProvider } from '../../providers/rest/rest';
 import { HaversineProvider } from '../../providers/haversine/haversine';
+import { Pollutions } from '../../models/pollutions';
 
 /**
  * Generated class for the MyStationPage page.
@@ -21,37 +22,55 @@ import { HaversineProvider } from '../../providers/haversine/haversine';
 export class MyStationPage {
 
   stations: StationObj[] = [];
-
+  pollution: Pollutions[];
+  station: StationObj;
   constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation, public restProvider: RestProvider, public haversineService: HaversineProvider) {
-    
-   
-  }
-
-  getNearestPollution(){
-    let nearestStation = this.getNearestStation();
+    this.restProvider.getTab().then((data: StationObj[]) => {
+      this.stations = data;
+      this.station = this.stations[0];
+    let myLocalization: any;
+    this.getMyLocalization().then(data => {
+      myLocalization = data;
+      let min = this.getInMeters(myLocalization, new GeoCoord(this.station.latitude, this.station.longitude));
+        for (let i = 1; i < this.stations.length; i++){
+          let state = this.stations[i];
+          let value = this.getInMeters(myLocalization, new GeoCoord(state.latitude, state.longitude))
+          if (value < min){
+            min = value;
+            this.station = state;
+          }
+        }
+        this.restProvider.getSingleStation(this.station.id).then((data: Pollutions[]) => {
+          this.pollution = data;
+         })
+       })
+    })
   }
 
   getNearestStation(){
     let station: StationObj = this.stations[0];
-    let myLocalization = this.getMyLocalization();
-    let max = this.getInMeters(myLocalization, new GeoCoord(station.latitude, station.longitude));
-    for (let i = 1; i < this.stations.length; i++){
-      let state = this.stations[i];
-      let value = this.getInMeters(myLocalization, new GeoCoord(state.latitude, state.longitude))
-      if (value > max){
-        max = value;
-        station = state;
-      }
-    }
-    return station;
+    let myLocalization: any;
+    this.getMyLocalization().then(data => {
+      myLocalization = data;
+      let max = this.getInMeters(myLocalization, new GeoCoord(station.latitude, station.longitude));
+        for (let i = 1; i < this.stations.length; i++){
+          let state = this.stations[i];
+          let value = this.getInMeters(myLocalization, new GeoCoord(state.latitude, state.longitude))
+          if (value > max){
+            max = value;
+            station = state;
+          }
+        }
+        return station;
+    })
   }
 
   getMyLocalization(){
-    let my: GeoCoord;
-    this.geolocation.getCurrentPosition().then(pos => {
-      my = new GeoCoord(pos.coords.latitude, pos.coords.longitude);
+    return new Promise(resolve => {
+      this.geolocation.getCurrentPosition().then(pos => {
+        resolve(new GeoCoord(pos.coords.latitude, pos.coords.longitude));
+      })
     })
-    return my;
   }
 
   getInMeters(my: GeoCoord, stat: GeoCoord){
